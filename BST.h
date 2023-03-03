75d2177fd03a5d96b9ba43d599cecb2293bc4c74
narawit6513113 <narawit.pha@student.mahidol.edu>
2023-03-03 15:48:23 +0700

    1

--- BST.h
+++ BST.h
@@ -16,24 +16,22 @@ typedef struct {
 // prototypes
 
 void insertNode( BST *, int );
-/*
+
 void insertNode_R(TreeNodePtr * t,int value){
   if(t==NULL){
-   t=new TreeNode;
-	 if(t){
-		t->leftPtr=NULL;
-		t->rightPtr=NULL;
-		t->data=value;
+   *t = (TreeNodePtr)malloc(sizeof(struct TreeNode));
+   if(*t){
+		(*t)->leftPtr=NULL;
+		(*t)->rightPtr=NULL;
+		(*t)->data=value;
    }
   }
   else{
-     if(t->data>=value) //left
-        
-     else //right
-      
+     if((*t)->data>=value) insertNode_R(&((*t)->leftPtr),value);//left
+     else insertNode_R(&((*t)->rightPtr),value); //right
    }
 }
-*/
+
 void insertNode( BST *b, int value ){
 	 TreeNodePtr t =b->root,new_node;
 	 int inserted=0;
@@ -44,20 +42,27 @@ void insertNode( BST *b, int value ){
 		new_node->data=value;
 		/*First Node*/
 		if(!b->root)	b->root=new_node;
+
 		else {
 while(!inserted){
    if(t->data >=value){
      /* move/insert to the left*/
-    
-     }
-  	 
-  }
-   else{
-	      /* move/ insert to the right*/
-    
+    if(t->leftPtr==NULL)
+    {
+       t->leftPtr = new_node;
+       inserted = 1;
     }
-	}
-   
+    else t=t->leftPtr;
+     }
+   else/* move/ insert to the right*/
+   {
+      if(t->rightPtr==NULL)
+    {
+       t->rightPtr = new_node;
+       inserted = 1;
+    }
+    else (t=t->rightPtr);
+     }
   }//end while		
   }//end else;
   b->size++;
@@ -78,3 +83,13 @@ void inOrder( TreeNodePtr treePtr )
       inOrder( treePtr->rightPtr ); //Recursion to the right
    } // end if                          
 } // end 
+
+void preOrder( TreeNodePtr treePtr )
+{ 
+   // if tree is not empty, then traverse
+   if ( treePtr != NULL ) {        
+      printf("%3d",treePtr->data) ;  //print the value  
+      preOrder( treePtr->leftPtr ); //Recursion to the left
+      preOrder( treePtr->rightPtr ); //Recursion to the right
+   } // end if                          
+} 
